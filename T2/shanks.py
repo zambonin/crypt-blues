d82ecb63c3b85b0a89351b0f2606a5928fdda1a8
Gustavo Zambonin <gzmbnn@gmail.com>
2015-07-03 20:42:16 -0300

    after losing half the code...

---
+++ T2/shanks.py
@@ -0,0 +1,16 @@
+import utils
+
+def shanks_algorithm(alpha=6, beta=248388, p=458009):
+    m = int(p ** .5) + 1
+    y1 = alpha**m % p
+    y2 = utils.inv_mod(alpha, p)
+    frst_list, scnd_list = [], []
+
+    for i in range(m):
+        frst_list.append([i, y1**i % p])
+        scnd_list.append([i, beta * (y2**i) % p])
+
+    for i in frst_list:
+        for j in scnd_list:
+            if i[1] == j[1]:
+                return m*i[0] + j[0]
